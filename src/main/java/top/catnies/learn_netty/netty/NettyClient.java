dcb77a118c972dbbdc37240a97504e4c895242fe
Catnies <catnies@foxmail.com>
2025-05-22 14:26:53 +0800

    Netty 服务器和客户端的简易实现.

---
+++ src/main/java/top/catnies/learn_netty/netty/NettyClient.java
@@ -0,0 +1,55 @@
+package top.catnies.learn_netty.netty;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
+import java.util.concurrent.TimeUnit;
+
+public class NettyClient {
+    public static void main(String[] args) {
+        Bootstrap bootstrap = new Bootstrap();  // 创建客户端的 Bootstrap
+        NioEventLoopGroup group = new NioEventLoopGroup();
+        bootstrap.group(group);   // 客户端只需要一个线程组（EventLoopGroup），负责连接和处理读写。
+        bootstrap.channel(NioSocketChannel.class);  // 指定 Netty 客户端使用的通道类型. NioSocketChannel 是对 NIO 原生 SocketChannel 的封装.
+        bootstrap.handler(new ChannelInitializer<SocketChannel>() {  // 为 ServerBootstrap 添加一个子处理器
+            @Override
+            protected void initChannel(SocketChannel socketChannel) throws Exception {
+                ChannelPipeline pipeline = socketChannel.pipeline(); // 获取管道
+
+                // 出站处理器
+                pipeline.addLast(new StringEncoder()); // 字符串编码成Buffer
+
+                // 入站处理器
+                pipeline.addLast(new StringDecoder()); // Buffer解码成字符串
+                pipeline.addLast(new PrintHandler());   // 自定义打印解码处理器
+
+            }
+        });
+
+
+        ChannelFuture connectFuture = bootstrap.connect("127.0.0.1", 8987); // 指定Bootstrap的目标端口.
+        connectFuture.addListener(connect -> {
+            if (connect.isSuccess()) {
+                System.out.println("客户端成功链接了 8987 端口.");
+                EventLoop eventLoop = connectFuture.channel().eventLoop();  // 获取channel的eventloop, 因为客户端和服务端是1对1的
+                eventLoop.scheduleAtFixedRate(() -> {   // 每隔一秒定时向服务端发送消息
+                    connectFuture.channel().writeAndFlush("hello server! at " + System.currentTimeMillis());
+                }, 0, 1, TimeUnit.SECONDS);
+            }
+        });
+    }
+
+
+
+    static class PrintHandler extends SimpleChannelInboundHandler<String> {
+        @Override
+        protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+            System.out.println("收到来自服务器: " + ctx.channel().remoteAddress() + " 的消息, 内容是: " + msg);
+        }
+    }
+}
---
+++ src/main/java/top/catnies/learn_netty/netty/NettyServer.java
@@ -0,0 +1,98 @@
+package top.catnies.learn_netty.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
+
+public class NettyServer {
+    public static void main(String[] args) {
+        ServerBootstrap serverBootstrap = new ServerBootstrap(); // 创建服务器的 Bootstrap .
+        EventLoopGroup bossGroup = new NioEventLoopGroup(); // 第一个是 bossGroup，用于接收连接；
+        EventLoopGroup workerGroup = new NioEventLoopGroup();  // 第二个是 workerGroup，用于处理每个连接的数据读写。
+        serverBootstrap.group(bossGroup, workerGroup); // 为 Bootstrap 分配 Boss组 和 Worker组 .
+        serverBootstrap.channel(NioServerSocketChannel.class); // 指定 Netty 服务端使用的通道类型. NioServerSocketChannel 是对 NIO 原生 ServerSocketChannel 的封装.
+        serverBootstrap.childHandler(new ChannelInitializer<SocketChannel>() {  // 设置每一个新连接（子通道）的初始化逻辑，添加入站和出站处理器
+            @Override
+            protected void initChannel(SocketChannel socketChannel) throws Exception {
+                ChannelPipeline pipeline = socketChannel.pipeline(); // 获取管道
+
+                // 入站处理器
+                pipeline.addLast(new StringDecoder()); // Buffer解码成字符串
+                pipeline.addLast(new StatusHandler()); // 自定义状态处理器
+                pipeline.addLast(new PrintHandler());  // 自定义打印解码处理器
+                pipeline.addLast(new ResponseHandler()); // 自定义回复消息
+
+                // 出站处理器
+                pipeline.addLast(new StringEncoder()); // 字符串编码成Buffer
+
+            }
+        });
+
+
+        ChannelFuture bindFuture = serverBootstrap.bind(8987); // 让Bootstrap监听端口, 返回的是一个future
+        bindFuture.addListener( future -> {     // 可以通过添加监听器, 来检查监听端口是否成功和失败, 然后执行相关的操作.
+            if (bindFuture.isSuccess()) System.out.println("服务端监听端口 8987 成功!");
+            else System.out.println("服务器监听端口失败, 请检查端口是否已经被占用!");
+        });
+    }
+
+
+    static class PrintHandler extends SimpleChannelInboundHandler<String> {
+        @Override
+        protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+            System.out.println("收到来自客户端: " + ctx.channel().remoteAddress() + " 的消息, 内容是: " + msg);
+            // SimpleChannelInboundHandler 会在 channelRead0 完成后自动释放 ByteBuf，所以这里要用 fireChannelRead 手动向下传递
+            ctx.fireChannelRead(msg); // 如果需要把消息继续往下处理, 则需要手动显示传递.
+        }
+    }
+
+
+    static class ResponseHandler extends SimpleChannelInboundHandler<String> {
+        @Override
+        protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+            ctx.channel().writeAndFlush("回复消息 -> " + msg);
+        }
+
+
+    }
+
+    static class StatusHandler extends SimpleChannelInboundHandler<String> {
+        @Override
+        protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+            ctx.fireChannelRead(msg); // 直接传递给下一层处理
+        }
+
+        @Override
+        public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+            System.out.println(ctx.channel() + " 注册了");
+        }
+
+        @Override
+        public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+            System.out.println(ctx.channel() + " 解除注册了");
+        }
+
+        @Override
+        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+            System.out.println(ctx.channel() + " 可以使用了");
+        }
+
+        @Override
+        public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+            System.out.println(ctx.channel() + " 不可使用了");
+        }
+
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+            System.out.println(ctx.channel() + " 出现了异常");
+            cause.printStackTrace(); // 打印堆栈，有助于调试
+        }
+    }
+}
+
+
+
